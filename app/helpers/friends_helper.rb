4737836c4833adbdda58b231fc78853d1037f1dc
DevangBajpai <devangbajpai7@gmail.com>
2022-01-15 22:09:09 +0530

    first commit

---
+++ app/helpers/friends_helper.rb
@@ -0,0 +1,11 @@
+module FriendsHelper
+	def after_sign_in_path_for(resource)
+ if current_user.has_role?(:admin)
+  dashboard_path
+ elsif current_user.has_role?(:student)
+  root_path
+ else
+  root_path
+ end
+end
+end
